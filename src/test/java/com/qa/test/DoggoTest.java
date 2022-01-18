88639d1a9afa27684e9350c3fa74b71fa35b4456
ibraheem nasser <ibraheemalialkaby@gmail.com>
2022-01-18 17:20:21 +0000

    Test prelim

--- src/test/java/com/qa/test/DoggoTest.java
+++ src/test/java/com/qa/test/DoggoTest.java
@@ -1,5 +1,16 @@
 package com.qa.test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.qa.Doggo;
+
 public class DoggoTest {
+
+	@Test
+	public void no15th() {
+		assertEquals(15, Doggo.doggo(15));
+	}
 
 }
